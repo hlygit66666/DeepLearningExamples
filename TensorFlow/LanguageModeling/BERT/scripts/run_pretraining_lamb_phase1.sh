721814cf4a98f1973b5bfc2c479880981a42aa6c
hlygit66666 <32728786+hlygit66666@users.noreply.github.com>
2021-08-22 21:25:45 +0800

    Update run_pretraining_lamb_phase1.sh

--- TensorFlow/LanguageModeling/BERT/scripts/run_pretraining_lamb_phase1.sh
+++ TensorFlow/LanguageModeling/BERT/scripts/run_pretraining_lamb_phase1.sh
@@ -89,7 +89,7 @@ for DIR_or_file in $DATA_DIR $RESULTS_DIR_PHASE1 $BERT_CONFIG; do
   fi
 done
 
- $mpi python /workspace/bert/run_pretraining.py \
+ $mpi python /root/paddlejob/bert/run_pretraining.py \
      --input_files_dir=$INPUT_FILES \
      --eval_files_dir=$EVAL_FILES \
      --output_dir=$RESULTS_DIR_PHASE1 \
